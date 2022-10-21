d22fd8f40d1929a9e47e15c0b30984c80048cbdd
Gajus Kuizinas <gajus@gajus.com>
2022-10-20 20:46:16 -0500

    style: remove unused variable

--- test/roarr-sentry/factories/createRoarrSentryIntegration.ts
+++ test/roarr-sentry/factories/createRoarrSentryIntegration.ts
@@ -1,48 +1,43 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import test from 'ava';
-import {
-  ROARR,
-} from 'roarr';
-import * as sinon from 'sinon';
-import {
-  createRoarrSentryIntegration,
-} from '../../../src/factories/createRoarrSentryIntegration';
+import test from "ava";
+import { ROARR } from "roarr";
+import * as sinon from "sinon";
+import { createRoarrSentryIntegration } from "../../../src/factories/createRoarrSentryIntegration";
 
-test('creates class with CaptureRoarr constructor name', (t) => {
-  t.is(createRoarrSentryIntegration({
-    addBreadcrumb: () => {},
-  }).constructor.name, 'CaptureRoarr');
+test("creates class with CaptureRoarr constructor name", (t) => {
+  t.is(
+    createRoarrSentryIntegration({
+      addBreadcrumb: () => {},
+    }).constructor.name,
+    "CaptureRoarr"
+  );
 });
 
-const noop = (): any => {};
-
-test('overrides ROARR.write method', (t) => {
+test("overrides ROARR.write method", (t) => {
   const originalWrite = ROARR.write;
 
   createRoarrSentryIntegration({
     addBreadcrumb: () => {},
-  })
-    .setupOnce();
+  }).setupOnce();
 
   t.not(ROARR.write, originalWrite);
 });
 
-test('passes-through calls to ROARR.write', (t) => {
+test("passes-through calls to ROARR.write", (t) => {
   const spy = sinon.stub();
 
   ROARR.write = spy;
 
   createRoarrSentryIntegration({
     addBreadcrumb: () => {},
-  })
-    .setupOnce();
+  }).setupOnce();
 
   const payload = JSON.stringify({
     context: {
-      namespace: 'bar',
+      namespace: "bar",
     },
-    message: 'foo',
+    message: "foo",
   });
 
   ROARR.write(payload);
@@ -50,34 +45,33 @@ test('passes-through calls to ROARR.write', (t) => {
   t.true(spy.called);
 });
 
-test('adds logs to breadcrumbs', (t) => {
+test("adds logs to breadcrumbs", (t) => {
   const addBreadcrumb = sinon.stub();
 
   ROARR.write = () => {};
 
   createRoarrSentryIntegration({
     addBreadcrumb,
-  })
-    .setupOnce();
+  }).setupOnce();
 
   const payload = JSON.stringify({
     context: {
-      namespace: 'bar',
+      namespace: "bar",
     },
-    message: 'foo',
+    message: "foo",
   });
 
   ROARR.write(payload);
 
   t.like(addBreadcrumb.firstCall.firstArg, {
-    category: 'bar',
+    category: "bar",
     data: {
       context: {
-        namespace: 'bar',
+        namespace: "bar",
       },
     },
-    level: 'error',
-    message: 'foo',
-    type: 'default',
+    level: "error",
+    message: "foo",
+    type: "default",
   });
 });
